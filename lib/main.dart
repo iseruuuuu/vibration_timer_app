dec72f6228c1ed6b262ea59c7aa42a18bb37f3bc
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-09-20 00:29:11 +0900

    add: localizationを追加(未完成)

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 // Project imports:
 import 'package:vibration_timer/title/title_ful_screen.dart';
+import 'package:vibration_timer/language/japanese_cupertino_localizations.dart'
+    as jcl; //(ほかのライブラリと競合したのでas jclとしている)
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +20,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      // localizationsDelegates: [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   DefaultCupertinoLocalizations.delegate,
+      //   jcl.JapaneseCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: [
+      //   Locale('en', 'US'),
+      //   Locale('ja', 'JP'),
+      // ],
+      // locale: Locale('ja', 'JP'),
       debugShowCheckedModeBanner: false,
       home: TitleFulScreen(),
     );
